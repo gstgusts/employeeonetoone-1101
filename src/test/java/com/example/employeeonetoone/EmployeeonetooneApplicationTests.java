770b0401b5d4ca754040a0366fff61c424eb8c85
Gusts Linkevics <gusts@gst.lv>
2021-04-08 20:04:13 +0300

    Initial commit

---
+++ src/test/java/com/example/employeeonetoone/EmployeeonetooneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeonetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeonetooneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
